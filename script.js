e445a4cf40118131e3567048df4599f89ee65be9
Brane Marjanovic <marjanovicbrane93@gmail.com>
2021-10-14 17:03:43 +0200

    I added a new class .hidden in css file, because we want to hide a dice element.Now we can add this .hidden class to html class .dice to hide the dice.

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
 'use strict';
 
+//Selecting elements
+const score0El=document.querySelector('#score--0');
+const score1El=document.getElementById('score--1');
+//we want to hide this dice element by default
+const diceEl=document.querySelector('.dice');
+
+//setting elements to initial values
+score0El.textContent=0;
+score1El.textContent=0;
+//we added .hidden class to css file and we want now to add this hidden class to .dice class in html file
+diceEl.classList.add('hidden');
+
+
